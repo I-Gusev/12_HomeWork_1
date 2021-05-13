65a2ab9f6e0900c8984549539355715043a351a2
Igor <vse.supersky@gmail.com>
2021-05-13 17:18:11 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const numberOfFilms = +prompt("Сколько фильмов Вы посмотрели", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm1 = prompt("Один из последних просмотренных фильмов", ""),
+      score1 = prompt("на сколько оцениваете его", ""),
+      lastFilm2 = prompt("Один из последних просмотренных фильмов", ""),
+      score2 = prompt("на сколько оцениваете его", "");
+
+//  movies = {
+//      lastFilm: lastFilm, 
+//      logan: score
+//  };
+
+//  personalMovieDB.movies = movies;
+personalMovieDB.movies[lastFilm1] = score1;
+personalMovieDB.movies[lastFilm2] = score2;
+
+console.log(personalMovieDB);
